a82abd6f4be96d2bfee480d068d5a8e1ed9baf1b
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-07-07 13:42:41 +0530

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKStXMFBHiNHnCUjirto_SdCBMEE5Rujk",
+  authDomain: "school-management-projec-bd745.firebaseapp.com",
+  projectId: "school-management-projec-bd745",
+  storageBucket: "school-management-projec-bd745.appspot.com",
+  messagingSenderId: "928704676339",
+  appId: "1:928704676339:web:4f9f53a37700b6ab855286"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
